d46efab64e50351695abbf1ec0dc909c0e8ae8e2
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-20 09:58:59 +0530

    JwtConstant class created

---
+++ src/main/java/com/ecommerce/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.ecommerce.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY = "bdcdbvhdfjcxnbdcndcndbcnjdbcnjdcgdjxcvjxcvhjdcvdjchjsxvgdshxcjfdhshxjgvs";
+    public static final String JWT_HEADER="Authorization";
+}
