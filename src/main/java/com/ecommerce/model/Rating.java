f144b9f3edb142c754512cd6278191e57d9f8bbe
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-20 12:51:32 +0530

    Rating entity created

---
+++ src/main/java/com/ecommerce/model/Rating.java
@@ -0,0 +1,32 @@
+package com.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(name = "rating")
+    private double rating;
+
+    private LocalDateTime createdAt;
+
+}
