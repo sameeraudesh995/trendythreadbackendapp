d40e3fdaed370a6c3fbeff5ebb9fec08bd6fa8c7
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-20 12:48:31 +0530

    user entity create

---
+++ src/main/java/com/ecommerce/model/User.java
@@ -0,0 +1,46 @@
+package com.ecommerce.model;
+
+import ch.qos.logback.core.pattern.color.BoldYellowCompositeConverter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String email;
+    private String role;
+    private String mobile;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+
+    @ElementCollection
+    @CollectionTable(name = "payment_information", joinColumns = @JoinColumn(name = "user_id"))
+    private List<PaymentInformation> paymentInformations=new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Rating>ratings=new ArrayList<>();
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL,orphanRemoval = true)
+    private List<Review>reviews=new ArrayList<>();
+    private LocalDate createdAt;
+
+}
