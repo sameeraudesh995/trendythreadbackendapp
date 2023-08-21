483a6c3540786fefe57799fb1d2d69b7199391c7
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-21 15:32:14 +0530

    AuthController signup and signin created

--- src/main/java/com/ecommerce/controller/AuthController.java
+++ src/main/java/com/ecommerce/controller/AuthController.java
@@ -4,14 +4,19 @@ import com.ecommerce.config.JwtTokenProvider;
 import com.ecommerce.exception.UserException;
 import com.ecommerce.model.User;
 import com.ecommerce.repository.UserRepository;
+import com.ecommerce.request.LoginRequest;
 import com.ecommerce.response.AuthResponse;
+import com.ecommerce.service.CustomerUserServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,7 +31,10 @@ public class AuthController {
     private JwtTokenProvider jwtTokenProvider;
     @Autowired
     private PasswordEncoder passwordEncoder;
+    @Autowired
+    private CustomerUserServiceImplementation customerUserService;
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse>createUserHandler(@RequestBody User user) throws UserException {
 
         String email=user.getEmail();
@@ -58,5 +66,38 @@ public class AuthController {
 
 
 
+    }
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse>loginUserHandler(@RequestBody LoginRequest loginRequest){
+        String username=loginRequest.getEmail();
+        String password=loginRequest.getPassword();
+
+        System.out.println(username +" ----- "+password);
+
+        Authentication authentication = authenticate(username,password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token=jwtTokenProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Signin Success");
+
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
+
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails =customerUserService.loadUserByUsername(username);
+
+        System.out.println("sign in userDetails - "+userDetails);
+        if(userDetails==null){
+            throw new BadCredentialsException("Invalid Username...");
+        }
+        if(!passwordEncoder.matches(password, userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid username or password...");
+        }
+
+        return new UsernamePasswordAuthenticationToken(userDetails,null,userDetails.getAuthorities());
     }
 }
