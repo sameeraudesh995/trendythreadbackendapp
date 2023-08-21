0b5f768c8c3a9fd134f4af281cffdc6be8b96bee
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-21 15:33:20 +0530

    AuthResponse  created

--- src/main/java/com/ecommerce/response/AuthResponse.java
+++ src/main/java/com/ecommerce/response/AuthResponse.java
@@ -1,12 +1,38 @@
 package com.ecommerce.response;
 
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 public class AuthResponse {
     private String jwt;
     private String message;
 
+    public AuthResponse() {
+    }
+
     public AuthResponse(String jwt, String message) {
         super();
         this.jwt = jwt;
+        this.message = message;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public void setJwt(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
         this.message = message;
     }
 }
