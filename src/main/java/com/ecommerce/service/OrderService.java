7e5f3b5a570b3f356dd92857addf1bcf7547b51f
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 12:49:47 +0530

    OrderService created

---
+++ src/main/java/com/ecommerce/service/OrderService.java
@@ -0,0 +1,31 @@
+package com.ecommerce.service;
+
+import com.ecommerce.exception.OrderException;
+import com.ecommerce.model.Address;
+import com.ecommerce.model.Order;
+import com.ecommerce.model.User;
+
+import java.util.List;
+
+public interface OrderService {
+    public Order createOrder(User user, Address shippingAdress);
+
+    public Order findOrderById(Long orderId) throws OrderException;
+
+    public List<Order> usersOrderHistory(Long userId);
+
+    public Order placedOrder(Long orderId) throws OrderException;
+
+    public Order confirmedOrder(Long orderId)throws OrderException;
+
+    public Order shippedOrder(Long orderId) throws OrderException;
+
+    public Order deliveredOrder(Long orderId) throws OrderException;
+
+    public Order cancledOrder(Long orderId) throws OrderException;
+
+    public List<Order>getAllOrders();
+
+    public void deleteOrder(Long orderId) throws OrderException;
+
+}
