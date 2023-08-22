e563d2bc6423b5acb27a9bf7b6f4ce55609819ae
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 22:41:39 +0530

    ReviewRequest created

---
+++ src/main/java/com/ecommerce/request/ReviewRequest.java
@@ -0,0 +1,11 @@
+package com.ecommerce.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReviewRequest {
+    private Long productId;
+    private String review;
+}
