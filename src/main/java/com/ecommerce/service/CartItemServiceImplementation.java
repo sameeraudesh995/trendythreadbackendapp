359c992beabb4054f0a04285940bf7e385fa44d2
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 16:27:39 +0530

    override methods from cartItemService and CartItemServiceImplementation updated.

--- src/main/java/com/ecommerce/service/CartItemServiceImplementation.java
+++ src/main/java/com/ecommerce/service/CartItemServiceImplementation.java
@@ -5,10 +5,13 @@ import com.ecommerce.exception.UserException;
 import com.ecommerce.model.Cart;
 import com.ecommerce.model.CartItem;
 import com.ecommerce.model.Product;
+import com.ecommerce.model.User;
 import com.ecommerce.re.CartRepository;
 import com.ecommerce.repository.CartItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CartItemServiceImplementation implements CartItemService{
@@ -20,26 +23,54 @@ public class CartItemServiceImplementation implements CartItemService{
     private CartRepository cartRepository;
     @Override
     public CartItem createCartItem(CartItem cartItem) {
-        return null;
+        cartItem.setQuantity(1);
+        cartItem.setPrice(cartItem.getProduct().getPrice()*cartItem.getQuantity());
+        cartItem.setDiscountedPrice(cartItem.getProduct().getDiscountedPrice()*cartItem.getQuantity());
+
+        CartItem createdCartItem = cartItemRepository.save(cartItem);
+        return createdCartItem;
     }
 
     @Override
-    public CartItem updateCartItem(Long userId, CartItem cartItem) throws CartItemException, UserException {
-        return null;
+    public CartItem updateCartItem(Long userId, Long id, CartItem cartItem) throws CartItemException, UserException {
+        CartItem item= findCartItemById(id);
+        User user = userService.findUserById(item.getUserId());
+
+        if(user.getId().equals(userId)){
+            item.setQuantity(cartItem.getQuantity());
+            item.setPrice(item.getQuantity()*item.getProduct().getPrice());
+            item.setDiscountedPrice(item.getProduct().getDiscountedPrice()*item.getQuantity());
+
+        }
+        return cartItemRepository.save(item);
     }
 
     @Override
     public CartItem isCartItemExist(Cart cart, Product product, String size, Long userId) {
-        return null;
+        CartItem cartItem=cartItemRepository.isCartItemExist(cart,product,size,userId);
+        return cartItem;
     }
 
     @Override
     public void removecartItem(Long userId, Long cartItemId) throws CartItemException, UserException {
+        CartItem cartItem=findCartItemById(cartItemId);
+        User user=userService.findUserById(cartItem.getUserId());
+        User reqUser=userService.findUserById(userId);
+        if(user.getId().equals(reqUser.getId())){
+            cartItemRepository.deleteById(cartItemId);
 
+        }
+        else{
+            throw new UserException("you can't remove another users item");
+        }
     }
 
     @Override
     public CartItem findCartItemById(Long cartItemId) throws CartItemException {
-        return null;
+        Optional<CartItem>opt = cartItemRepository.findById(cartItemId);
+        if(opt.isPresent()){
+            return opt.get();
+        }
+      throw new CartItemException("cart Item not found with id : "+cartItemId);
     }
 }
