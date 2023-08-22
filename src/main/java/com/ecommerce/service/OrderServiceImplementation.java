09f08c0aca319dbbd6c007a6e560ef5f41e93d9e
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 13:28:47 +0530

    OrderServiceImplementation created

---
+++ src/main/java/com/ecommerce/service/OrderServiceImplementation.java
@@ -0,0 +1,71 @@
+package com.ecommerce.service;
+
+import com.ecommerce.exception.OrderException;
+import com.ecommerce.model.Address;
+import com.ecommerce.model.Order;
+import com.ecommerce.model.User;
+import com.ecommerce.re.CartRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderServiceImplementation implements OrderService{
+    @Autowired
+    private CartRepository cartRepository;
+    @Autowired
+    private CartItemService cartItemService;
+    @Autowired
+    private ProductService productService;
+
+    @Override
+    public Order createOrder(User user, Address shippingAdress) {
+        return null;
+    }
+
+    @Override
+    public Order findOrderById(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public List<Order> usersOrderHistory(Long userId) {
+        return null;
+    }
+
+    @Override
+    public Order placedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order confirmedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order shippedOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order deliveredOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public Order cancledOrder(Long orderId) throws OrderException {
+        return null;
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public void deleteOrder(Long orderId) throws OrderException {
+
+    }
+}
