fa1f6d297087f30feeef9286f865b074f6f07a22
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 14:26:24 +0530

    CartService updated

--- src/main/java/com/ecommerce/service/CartItemService.java
+++ src/main/java/com/ecommerce/service/CartItemService.java
@@ -1,4 +1,16 @@
 package com.ecommerce.service;
 
+import com.ecommerce.exception.CartItemException;
+import com.ecommerce.exception.UserException;
+import com.ecommerce.model.Cart;
+import com.ecommerce.model.CartItem;
+import com.ecommerce.model.Product;
+
 public interface CartItemService {
+    public CartItem createCartItem(CartItem cartItem);
+    public CartItem updateCartItem(Long userId, CartItem cartItem) throws CartItemException, UserException;
+    public CartItem isCartItemExist(Cart cart, Product product,String size, Long userId );
+    public void removecartItem(Long userId, Long cartItemId) throws CartItemException, UserException;
+    public CartItem findCartItemById(Long cartItemId) throws CartItemException;
+
 }
