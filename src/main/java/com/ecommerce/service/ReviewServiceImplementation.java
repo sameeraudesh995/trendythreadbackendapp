30aee6fcc6272da718349a72167ad5bdcb4a6638
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 22:42:20 +0530

    ReviewServiceImplementation interface created

---
+++ src/main/java/com/ecommerce/service/ReviewServiceImplementation.java
@@ -0,0 +1,41 @@
+package com.ecommerce.service;
+
+import com.ecommerce.exception.ProductException;
+import com.ecommerce.model.Product;
+import com.ecommerce.model.Review;
+import com.ecommerce.model.User;
+import com.ecommerce.repository.ProductRepository;
+import com.ecommerce.repository.ReviewRepository;
+import com.ecommerce.request.ReviewRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+@Service
+public class ReviewServiceImplementation implements ReviewService{
+    @Autowired
+    private ReviewRepository reviewRepository;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public Review createReview(ReviewRequest req, User user) throws ProductException {
+        Product product=productService.findProductById(req.getProductId());
+
+        Review review=new Review();
+        review.setUser(user);
+        review.setProduct(product);
+        review.setReview(req.getReview());
+        review.setCreatedAt(LocalDateTime.now());
+
+        return reviewRepository.save(review);
+    }
+
+    @Override
+    public List<Review> getAllReview(Long productId) {
+
+        return reviewRepository.getAllProductsReview(productId);
+    }
+}
