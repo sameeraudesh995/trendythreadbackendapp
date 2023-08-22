adc776fc22a4792ce8a4e5a8c38f8a5498bb753b
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-22 11:28:36 +0530

    CategoryRepository  created

---
+++ src/main/java/com/ecommerce/repository/CategoryRepository.java
@@ -0,0 +1,15 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    public Category findByName(String name);
+
+    @Query("SELECT  c from Category c WHERE c.name=:name AND c.parentCategory.name=:parentCategoryName")
+    public Category findByNameAndParent(@Param("name")String name, @Param("parentCategoryName")String parentCategoryName);
+}
