4217c209bf8fbec317bb2630d05d88eabe6a83f6
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-24 15:43:34 +0530

    openApi starter webMVC ui dependency setup , swagger added , pom updated

--- src/main/java/com/ecommerce/TrendythreadbackendappApplication.java
+++ src/main/java/com/ecommerce/TrendythreadbackendappApplication.java
@@ -1,9 +1,31 @@
 package com.ecommerce;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "trenndytreand Ecommerce Backend",
+				version = "1.0.0",
+				description = "This Ecommerce project only for learning",
+				termsOfService = "Education",
+				contact = @Contact(
+						name = "Sameera Udesh",
+						email = "sameeraudesh95@gmail.com"
+				),
+				license = @License(
+						name = "Licence",
+						url = "sameera"
+				)
+		)
+)
 public class TrendythreadbackendappApplication {
 
 	public static void main(String[] args) {
