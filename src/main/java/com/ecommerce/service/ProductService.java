1e08d2409a071f88bc5e316b5a4c6dd7879d1571
sameera udesh <73770245+sameeraudesh995@users.noreply.github.com>
2023-08-27 10:45:47 +0530

    missing productException added

--- src/main/java/com/ecommerce/service/ProductService.java
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -9,13 +9,14 @@ import java.util.List;
 
 public interface ProductService {
 
-    public Product createProduct(CreateProductRequest req);
+    public Product createProduct(CreateProductRequest req) throws ProductException;
     public  String deleteProduct(Long productId) throws ProductException;
     public Product updateProduct(Long productId, Product req)throws ProductException;
     public Product findProductById(Long id)throws ProductException;
     public List<Product>findProductByCategory(String category);
 
     public List<Product> getAllProducts();
+    public List<Product> searchProduct(String query);
 
     public Page<Product>getAllProduct(String category, List<String>color, List<String>sizes, Integer minPrice,
                                       Integer maxPrice, Integer miniDiscount, String sort,String stock, Integer pageNumber,
